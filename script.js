2686bd3c1eca998cf9b9a9c565b00524942f22a7
kiaraputrilia <kiaraputrilia@gmail.com>
2024-04-11 20:08:14 -0400

    made title tags move

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+  // Array of text to cycle through in the title
+  const titles = ["Syllabus", "A Growing Syllabus", "An Imperfect Syllabus", "An Ever-Expanding Syllabus", "A syllabus that's...", "Perpetually, In Fermentation"];
+  let index = 0;
+
+  // Function to update the title
+  function updateTitle() {
+    document.title = titles[index];
+    index = (index + 1) % titles.length; // Move to the next title, looping back to the start
+  }
+
+  // Update the title every 2 seconds
+  setInterval(updateTitle, 2000);
+
 document.addEventListener('DOMContentLoaded', function() {
     const mediaItems = document.querySelectorAll('#gallery .media-wrapper');
     let currentIndex = 0;
