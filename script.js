a05564790cb677a38bcd109c975a05f70a878341
kiaraputrilia <kiaraputrilia@gmail.com>
2024-04-11 21:59:24 -0400

    cleaned her up

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-  // Array of text to cycle through in the title
   const titles = ["Syllabus", "A Growing Syllabus", "An Imperfect Syllabus", "An Ever-Expanding Syllabus", "A syllabus that's...", "Perpetually, In Fermentation"];
   let index = 0;
 
-  // Function to update the title
   function updateTitle() {
     document.title = titles[index];
-    index = (index + 1) % titles.length; // Move to the next title, looping back to the start
+    index = (index + 1) % titles.length; 
   }
 
-  // Update the title every 2 seconds
   setInterval(updateTitle, 2000);
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -16,12 +13,10 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentIndex = 0;
   
     function showMedia(index) {
-      // Hide all media items
       mediaItems.forEach(item => {
         item.classList.remove('active');
       });
   
-      // Show the media item at the given index
       mediaItems[index].classList.add('active');
     }
   
@@ -30,10 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
       showMedia(currentIndex);
     }
   
-    // Show the first media item initially
     showMedia(currentIndex);
-  
-    // Listen for clicks on the gallery to go to the next item
     document.getElementById('gallery').addEventListener('click', nextMedia);
   });
   
